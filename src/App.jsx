abf6ff53b2b2f513dd38aea2180662708a817ca2
hyeone22 <seunghyeone2@gmail.com>
2024-05-23 21:20:57 +0900

    [Chore] 임시저장

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
 import Layout from './components/Layout/Layout.jsx';
+import Test from './components/Test.jsx';
 
 function App() {
   return (
     <>
-      <Layout>{/* 여기 넣으세요!@ */}</Layout>
+      <Layout>
+        {/* 여기 넣으세요!@ */}
+        <Test />
+        <Test />
+        <Test />
+        <Test />
+        <Test />
+        <Test />
+      </Layout>
     </>
   );
 }
