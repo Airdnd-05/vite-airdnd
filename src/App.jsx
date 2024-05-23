ed13c5b86569b41dd64b7a5c0b5e0877acac0c2e
ptokos <jiseokchoij@gmail.com>
2024-05-23 14:19:00 +0900

    [Feat]: 상세페이지 배너

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
+import DetailPageBanner from './components/Layout/DetailPageBanner.jsx';
 import Layout from './components/Layout/Layout.jsx';
 
 function App() {
   return (
     <>
-      <Layout>{/* 여기 넣으세요!@ */}</Layout>
+      <Layout>
+        <DetailPageBanner />
+      </Layout>
     </>
   );
 }
