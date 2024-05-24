b18faa79817cd5fba7d7838416e94f56a1c0db44
seoeunhyo <seoeunhyo99@gmail.com>
2024-05-24 23:35:06 +0900

    [Refactor] 호스트 리뷰가 없을 시 호스팅 시작 연도만 표기되도록 하기 위해 컴포넌트 분리

---
+++ src/components/hostDetail/BeginnerHostInfoBox.jsx
@@ -0,0 +1,23 @@
+import hostInfo from "../../host-detail.json"
+
+const currentDate = new Date()
+const YEAR = currentDate.getFullYear() 
+
+function BeginnerHostInfoBox(){
+
+    return (
+
+            <div className="w-80 h-[12rem] shadow-lg mt-10 ml-14  bg-[white] rounded-[21px] flex flex-col justify-center items-center">
+                <div className="w-24 h-24 bg-gray-200 rounded-full overflow-hidden ">
+                     <img src={hostInfo.hostProfile.hostImageUrl} alt="" className="object-cover w-full h-full "/>
+                </div>
+                <div className="text-[1.8rem] font-bold mb-[0.4rem]">{hostInfo.hostProfile.hostName}</div>
+                <div className=" font-bold text-xs" >{YEAR - hostInfo.hostProfile.hostExperience}년에 호스팅 시작</div>
+            <div>
+
+        </div>
+        </div>
+    )
+}
+
+export default BeginnerHostInfoBox
