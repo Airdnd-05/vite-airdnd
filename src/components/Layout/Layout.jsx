a9223cd43f6b55450505ebe7906bde2bb282d3b2
hyeone22 <seunghyeone2@gmail.com>
2024-05-25 15:06:34 +0900

    [Design] 레이아웃 수정 및 스타일 세부 조정

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,11 +4,13 @@ import Footer from './Footer.jsx';
 
 function Layout({ children }) {
   // 최상단에 크기 조정 px로 ! full로 잡고 안에 컨텐츠들을 수정한다.
+  // 현재는 margin을 8.5rem으로 설정해 두었지만 언제든지 수정 가능
+  // Header, Footer도 동일
   return (
-    <div className="flex flex-col">
+    <div className="w-full flex flex-col">
       <Header />
-      <main className="w-full h-full flex-grow px-40">
-        <div className="max-w-[1120px] m-auto">{children}</div>
+      <main className="w-full flex-grow ">
+        <div className="px-20 mx-[8.5rem]">{children}</div>
       </main>
       <Footer />
     </div>
